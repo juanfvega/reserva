7230f6604b45f3b06f07aaebc9e630556c022f2f
juanfvega <juanfedericovega@gmail.com>
2022-12-12 22:36:28 -0300

    add read/update/delete in customer

--- routers/customer.py
+++ routers/customer.py
@@ -1,15 +1,36 @@
 from fastapi import APIRouter
 from pydantic import BaseModel, EmailStr
+from typing import Union
 from services.customer import createCustomer
+from services.customer import readCustomer
+from services.customer import updateCustomer
+from services.customer import deleteCustomer
+
+
 
 customerRouter = APIRouter(prefix="/customer")
 
 
 class Customer(BaseModel):
-    name:str
-    age:int
-    email:EmailStr
+    name:Union[str, None] = None
+    age:Union[int, None] = None
+    email:Union[EmailStr, None] = None
 
 @customerRouter.post("/create")
 async def create_customer(data: Customer):
     return createCustomer(name=data.name, age=data.age, email=data.email)
+
+
+@customerRouter.get("/read/{customerId}")
+async def read_customer(customerId: int):
+    return readCustomer(customerId=customerId)
+
+
+@customerRouter.put("/update/{customerId}")
+async def update_customer(customerId: int, data:Customer):
+        return updateCustomer(customerId=customerId, name=data.name, age=data.age, email=data.email)
+
+
+@customerRouter.delete("/delete/{customerId}")
+async def delete_customer(customerId: int):
+        return deleteCustomer(customerId=customerId)
--- services/customer.py
+++ services/customer.py
@@ -1,6 +1,8 @@
 from pony.orm import *
 from models.databaseManager import DatabaseManager
 from pydantic import EmailStr
+from fastapi import HTTPException
+from typing import Union
 
 @db_session
 def createCustomer(name:str, age:int, email:EmailStr):
@@ -8,3 +10,48 @@ def createCustomer(name:str, age:int, email:EmailStr):
     #table = DatabaseManager.getTable(table)
     newCustomer = customer(name=name, age=age, email=email)
     return newCustomer.to_dict()
+
+
+@db_session
+def readCustomer(customerId:int):
+    customer = DatabaseManager.getTable('Customer')
+    query = select(c for c in customer if c.id == customerId)
+    
+    if(query.exists()):
+        return query.first().to_dict()
+    else:
+        return HTTPException(status_code=404, detail='Customer not found')
+
+
+@db_session
+def updateCustomer(customerId:int, name:Union[str, None], age:Union[int, None], email:Union[EmailStr, None]):
+    customer = DatabaseManager.getTable('Customer')
+    query = select(c for c in customer if c.id == customerId)
+    if(query.exists()):        
+        customerObj = query.first()
+        if name:
+            print('in name')
+            customerObj.name = name
+        if age:
+            print('in age')
+            customerObj.age = age
+        if email: 
+            print('in email')
+            customerObj.email = email
+        
+        return customerObj.to_dict()
+    else:
+        return HTTPException(status_code=404, detail='Customer not found')
+        
+
+@db_session
+def deleteCustomer(customerId: int):
+    customer = DatabaseManager.getTable('Customer')
+    query = select(c for c in customer if c.id == customerId)
+    if query.exists():
+        customerDeleted = query.first()
+        customerDeleted.delete()
+        return customerDeleted.id
+    else:
+        return HTTPException(status_code=404, detail='Customer not found')
+    
